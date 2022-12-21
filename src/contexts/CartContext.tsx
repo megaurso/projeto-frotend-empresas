2ac461705c5496cf8b24daec7420a258c567d137
megaurso <bernadogui@hotmail.com>
2022-12-21 13:50:11 -0300

    fix: cart bug

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -3,14 +3,14 @@ import { toast } from "react-toastify";
 import { iChildren, iProducts } from "../interface";
 
 export interface iCartContext {
-  objectProduct: [] | iProducts[]
-  setObjectProduct: React.Dispatch<React.SetStateAction<iProducts[]>>
-  handleProduct: (elem: iProducts) => void
-  handleProductRemove: (elem: iProducts) => void
-  total: number
-  removeAll: () => void
-  removeProduct: (item: number) => void
-  counterCart:number
+  objectProduct: [] | iProducts[];
+  setObjectProduct: React.Dispatch<React.SetStateAction<iProducts[]>>;
+  handleProduct: (elem: iProducts) => void;
+  handleProductRemove: (elem: iProducts) => void;
+  total: number;
+  removeAll: () => void;
+  removeProduct: (item: number) => void;
+  counterCart: number;
 }
 
 export const CartContext = createContext<iCartContext>({} as iCartContext);
@@ -19,60 +19,71 @@ export const CartProvider = ({ children }: iChildren) => {
   const [objectProduct, setObjectProduct] = useState<iProducts[]>(
     [] as iProducts[]
   );
-  const [counterCart, setCounterCart] = useState(0)
+  const [counterCart, setCounterCart] = useState(0);
 
-  const increaseCart = () =>{
-    setCounterCart(counterCart+1)
-  }
+  const increaseCart = () => {
+    setCounterCart(counterCart + 1);
+  };
 
-  const decreaseAll = () =>{
-    setCounterCart(0)
-  }
+  const decreaseAll = () => {
+    setCounterCart(0);
+  };
 
-  const decreaseCart = () =>{
-    setCounterCart(counterCart - 1)
-  }
+  const decreaseCart = () => {
+    setCounterCart(counterCart - 1);
+  };
 
-  const total = objectProduct.reduce((acc, actual) => acc + (actual.price * actual.counter), 0);
-  
+  const total = objectProduct.reduce(
+    (acc, actual) => acc + actual.price * actual.counter,
+    0
+  );
+
   const handleProduct = (elem: iProducts) => {
     const findItem = objectProduct.find((item) => item.id === elem.id);
     if (findItem) {
-      const findIndex = objectProduct.findIndex((item) => item.id === elem.id);
-      const product = objectProduct.splice(findIndex,1)
-      product[0].counter += 1
-      setObjectProduct([product[0], ...objectProduct]);
-    }else{
-      increaseCart()
-      elem.counter = 1
+      const newArray = objectProduct.map((item) => {
+        if (item.id === elem.id) {
+          item.counter += 1;
+          return item;
+        } else {
+          return item;
+        }
+      });
+      setObjectProduct(newArray);
+    } else {
+      increaseCart();
+      elem.counter = 1;
       setObjectProduct([elem, ...objectProduct]);
-
     }
   };
 
   const handleProductRemove = (elem: iProducts) => {
     const findItem = objectProduct.find((item) => item.id === elem.id);
     if (findItem && elem.counter > 1) {
-      const findIndex = objectProduct.findIndex((item) => item.id === elem.id);
-      const product = objectProduct.splice(findIndex,1)
-      product[0].counter -= 1
-      setObjectProduct([product[0], ...objectProduct]);
+      const newArray = objectProduct.map((item) => {
+        if (item.id === elem.id) {
+          item.counter -= 1;
+          return item;
+        } else {
+          return item;
+        }
+      });
+      setObjectProduct(newArray);
     }
   };
 
   const removeAll = () => {
     setObjectProduct([]);
-    decreaseAll()
+    decreaseAll();
     toast.success("Todos os itens removidos da lista de compras");
   };
 
   const removeProduct = (id: number) => {
     const newList = objectProduct.filter((lis) => lis.id !== id);
-    decreaseCart()
+    decreaseCart();
     setObjectProduct(newList);
     toast.success("Produto removido");
   };
-
 
   return (
     <CartContext.Provider
@@ -84,7 +95,7 @@ export const CartProvider = ({ children }: iChildren) => {
         removeAll,
         removeProduct,
         counterCart,
-        handleProductRemove
+        handleProductRemove,
       }}
     >
       {children}
